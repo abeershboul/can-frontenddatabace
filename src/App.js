8b767153db4d008a6c8154c26738ea109b6239fa
abeershboul <abeer.shnoul@yahoo.com>
2022-09-09 01:25:37 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//import "./book.css"
+class BestBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    }
+  }
 
-export default App;
+  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+
+  componentDidMount = () => {
+    axios
+    .get(`http://localhost:3033/books`)
+    .then(result =>{
+      console.log(result.data);
+      this.setState({
+        books : result.data
+      })
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+    
+  }
+  render() {
+
+    /* TODO: render all the books in a Carousel */
+    return (
+      <>
+      
+      <Carousel  >
+        {this.state.books.length ? (
+           this.state.books.map((item) => {
+                return(
+                <Carousel.Item >
+                  <img
+                    className="d-block w-100"
+                    src="https://play-lh.googleusercontent.com/DmpYQrVcldrDuz5uyATqGbNvTALsJ1Bg3fpXM0p-VsRNM19osEB9-_ByvdjSbTvZQg=w450-h300-rw"
+                    alt={item.title}
+                  />
+                  <Carousel.Caption>
+                    <h3><p>{item.title}</p></h3>
+                    <p>{item.description}</p>
+                    <p>{item.status}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                )
+              })
+           
+          
+        ) : (
+          <h3>No Books Found :(</h3>
+        )}
+         </Carousel>
+      </>
+    );
+      }
+  }
+
+export default BestBooks;
